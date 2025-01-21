879493fe3e31b8a031a29f0d1a7bd1720192261d
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2025-01-21 12:25:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="bluesky5f61dce7fa034",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     install_requires=[
         "exorde_data",
